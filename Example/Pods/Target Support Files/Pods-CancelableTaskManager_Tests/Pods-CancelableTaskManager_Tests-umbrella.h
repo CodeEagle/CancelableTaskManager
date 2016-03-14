9b9ecaf009ed448f4f4a6696bc9d5efed601a354
CodeEagle <stasura@hotmail.com>
2016-03-14 17:50:28 +0800

    remove pods

--- Example/Pods/Target Support Files/Pods-CancelableTaskManager_Tests/Pods-CancelableTaskManager_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CancelableTaskManager_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CancelableTaskManager_TestsVersionString[];
-
