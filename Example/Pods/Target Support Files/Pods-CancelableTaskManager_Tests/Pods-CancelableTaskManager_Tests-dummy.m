5787578b8162ebc5d693558f3e453389db9f9f4e
CodeEagle <stasura@hotmail.com>
2016-03-14 17:49:33 +0800

    initialize

---
+++ Example/Pods/Target Support Files/Pods-CancelableTaskManager_Tests/Pods-CancelableTaskManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CancelableTaskManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CancelableTaskManager_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-CancelableTaskManager_Tests/Pods-CancelableTaskManager_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CancelableTaskManager_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CancelableTaskManager_TestsVersionString[];
+
