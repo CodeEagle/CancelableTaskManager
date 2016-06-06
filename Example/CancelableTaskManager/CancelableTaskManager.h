b48313b41291f4fe5bb221f8e1f76c2ddb5e214f
CodeEagle <stasura@hotmail.com>
2016-06-06 11:53:55 +0800

    support for carthage

---
+++ Example/CancelableTaskManager/CancelableTaskManager.h
@@ -0,0 +1,19 @@
+//
+//  CancelableTaskManager.h
+//  CancelableTaskManager
+//
+//  Created by LawLincoln on 16/6/6.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CancelableTaskManager.
+FOUNDATION_EXPORT double CancelableTaskManagerVersionNumber;
+
+//! Project version string for CancelableTaskManager.
+FOUNDATION_EXPORT const unsigned char CancelableTaskManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CancelableTaskManager/PublicHeader.h>
+
+
